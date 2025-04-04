40b230c8216d1875b6acd65d436456ad3287e1ab
BenGuerre <benjamin.guerre36260@gmail.com>
2025-04-04 15:00:12 +0200

    fix: Improve authentication check in CanAccessMiddleware

--- backend/microServBase/middlewares/roleMidleware.go
+++ backend/microServBase/middlewares/roleMidleware.go
@@ -7,13 +7,12 @@ import (
 
 // @Security BearerAuth
 func CanAccessMiddleware(ctx *gin.Context, rolesAllowed ...int32) bool {
-	currentUser := session.GetUserSession(ctx)
-	if currentUser.IDUser == 0 {
+	currentUser, err := session.GetUserSession(ctx)
+	if err != nil || currentUser.IDUser == 0 {
 		ctx.JSON(403, "You are not authenticated")
 		ctx.Abort()
 		return false
 	}
-
 	for _, roleId := range rolesAllowed {
 		if currentUser.IDRole == roleId {
 			ctx.Next()
